e5d15798298e0795113b7dd9b02f229af8e3f3eb
Shihaab <roushi02@gmail.com>
2020-04-15 18:15:20 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Shihaab Rouine</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}"/>
+    </head>
+    <body>
+        <h1>hello world</h1>
+    </body>
+</html>
