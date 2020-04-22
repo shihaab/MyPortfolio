661238baa697aa4f78f8f8bdda8a9b33b1084648
Shihaab <roushi02@gmail.com>
2020-04-22 20:51:04 +0200

    kleine aanpassingen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Portfolio showcasing my learning outcomes">
+        <meta name="keywords" content="Portfolio, learning outcome, shihaab, rouine">
+        <meta name="author" content="Shihaab Rouine">
 
         <title>Shihaab Rouine</title>
 
@@ -13,6 +16,7 @@
     </head>
     <body>
         <div class="wrapper-a">
+            <a href="#going-further" class="scroll-down">look further</a>
             <ul class="nav">
                 <li>
                     <a href="">Curricula Vitae</a>
@@ -26,11 +30,11 @@
             </ul>
             <div class="landing-title"><p>Shihaab</p> <p class="b">Rouine</p></div>
         </div>
-        <div class="wrapper-b">
+        <div class="wrapper-b" id="going-further">
             <!-- <div class="quoteme">
                 <p>Something that describes me</p>
             </div> -->
-            <p>Something that describes me</p>
+            <p>Something that describes me.</p>
         </div>
         <div class="wrapper-c">
             <!-- interactive media -->
@@ -49,6 +53,10 @@
                 </a>
                 <a href="development" class="outcome-secondimage" id="development-secondimage"></a>
             </div> -->
+        </div>
+        <span class="cursor"></span>
+        <div class="footer">
+            <div>the never ending cycle of learning</div>
         </div>
     </body>
 </html>
