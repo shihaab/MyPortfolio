dce88be770f85362139332cddd6eee590050b4f2
Shihaab <roushi02@gmail.com>
2020-06-20 20:48:00 +0200

    design gevuld met inhoud

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -17,21 +17,49 @@
             <h1>design</h1>
         </div>
 
-        <div class="scrolling-wrapper" id="wrapper-1">
+        <div style="overflow: hidden;" class="scrolling-wrapper" id="wrapper-1">
             <div class="card">
                 <div class="commentary">
                     <div class="commentary-inner">
-                        <h2 class="commentary-text" style="font-size: 7vw;">Visual designs</h2>
-                        <div style="background-image: url({{ asset('assets/img/compressed/IMG_0522.JPG') }});" class="commentary-firstimage"></div>
-                        <div style="background-image: url({{ asset('assets/img/compressed/IMG_0487.JPG') }});" class="commentary-secondimage"></div>
+                        <h2 class="commentary-text" style="font-size: 5vw;">Professionele toolset</h2>
+                        <div style="background-image: url(https://www.adobe.com/content/dam/offers-homepage/us/en/homepage/lobby-acrobat-032320-mobile-1440x810.jpg.img.jpg);" class="commentary-firstimage"></div>
+                        <div style="background-image: url({{ asset('assets/img/xd.svg') }});" class="commentary-secondimage"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-wrapper">
+            <div class="text-inner">
+                <p>
+                    Voor het maken van de visuele ontwerpen heb ik de producten van Adobe Creative Cloud gebruikt: After Effects, Premiere Pro, Media Encoder, Photoshop en XD.
+                    <br><br>
+                    Voor webdevelopment gebruik ik Visual Studio Code, ik vind dit een fijn programma waar veel plugins voor zijn maar ook zonder is het super fijn om te gebruiken.
+                    <br>
+                    Verder ben ik bekent met Illustrator, Dimension, Audition, Dreamweaver en Fuse CC.
+                </p>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <h1>Visuele ontwerpen</h1>
+                <p>Hier zijn twee Instagram story’s die ik heb gemaakt in Premiere Pro voor de specialisatie mediatechnieken, toen ik begon had ik de opdracht verkeerd gelezen en dacht ik dat de video max 50 seconden mocht duren (moest eigenlijk dus 15 seconden). Toen ik dit door had, had ik de video al geëxporteerd. </p>
+            </div>
+        </div>
+        <div class="scrolling-wrapper" id="wrapper-2">
+            <div class="card">
+                <div class="commentary">
+                    <div class="commentary-inner">
+                        <div class="commentary-video">
+                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/IPdYlKKVFEQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="card">
                 <div class="commentary">
                     <div class="commentary-inner">
-                        <h2 class="commentary-text">MyBrogrammers - internal project at my secondment agency</h2>
-                        <p class="commentary-p">The following page is a clickable wireframe meant for the stakeholders of this project</p>
+                        <h2 class="commentary-text">15s versie rechts</h2>
+                        <p class="commentary-p">Ik moest dus de video inkorten en had nu ook de kans om andere aanpassingen te maken. Ik vond de video wel spannend maar te vrolijk voor wat ik wilde bereiken. Daarom heb ik voor de tweede versie een minder agressieve kleurencorrectie gekozen zodat het wat depressiever overkomt. </p>
                     </div>
                 </div>
             </div>
@@ -39,40 +67,25 @@
                 <div class="commentary">
                     <div class="commentary-inner">
                         <div class="commentary-video">
-                            <iframe style="width: 100%; height: 100%;" src="https://xd.adobe.com/embed/f1b96907-bb8b-4afa-4801-f71cf58459ce-8788/" frameborder="0" allowfullscreen></iframe>
+                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/13WU97gQedQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <p>Voor de specialisatie mediatechnieken werd ook gevraagd om je kennis met After Effects aan te tonen door de 3D camera tracking functie te gebruiken om een stuk tekst in de video te plaatsen. Hier heb ik twee video’s voor gemaakt. De eerste is een video met een simpel stukje tekst dat vaststaat alsof het in de ruimte zweeft. De andere drie zijn samen een proces om een stuk tekst achter de persoon op de muur te zetten.</p>
+                <br><p>Let op: de eerste video hieronder is een playlist.</p>
+            </div>
+        </div>
         <div class="scrolling-wrapper" id="wrapper-2">
-        <span style="
-        position: absolute;
-        bottom: 91px;
-        left: 120px;
-        font-size: 2em;
-        font-weight: bold;
-        font-family: monospace;
-        z-index: 1;
-        transform: scale(1.5);
-        opacity: .5;
-        "><kbd style="
-        color: white;
-        background-color: black;
-        border-radius: 5px;
-        padding: 5px 7px;
-        ">Shift</kbd> + <kbd style="
-        color: white;
-        background-color: black;
-        border-radius: 5px;
-        padding: 5px 7px;
-        ">Scroll</kbd></span>
             <div class="card">
                 <div class="commentary">
                     <div class="commentary-inner">
-                        <h2 class="commentary-text">Borderline - digital strategy game</h2>
-                        <div style="background-image: url({{ asset('assets/img/dansman3.png') }});" class="commentary-firstimage"></div>
-                        <div style="background-image: url({{ asset('assets/img/dansman2.png') }});" class="commentary-secondimage"></div>
+                        <div class="commentary-video">
+                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/playlist?list=PLNq6lVSvI4Vt_JHpIYu3LS0FhTQwKAj3m&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -80,8 +93,53 @@
                 <div class="commentary">
                     <div class="commentary-inner">
                         <div class="commentary-video">
-                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/qF-vTw27VHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/VYGR2SaX1FY?mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <p>Toen ik net begon met het bedenken van een portfolio design heb ik wat ideeen kunnen documenteren. Dit heb ik toen gedaan voor de eerste portfolio review. </p>
+            </div>
+        </div>
+        <div class="pdf-wrapper p-top">
+            <div class="pdf-inner">
+                <h1 class="pdf-text"><a class="link" target="_blank" href="/assets/files/Portfolio%20Proces%20Shihaab%20Rouine%20v3.pdf">Portfolio proces</a></h1>
+                <iframe style="height: 90vh;width:100%;" src="/assets/files/Portfolio%20Proces%20Shihaab%20Rouine%20v3.pdf"></iframe>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <p>Voor de specialisatie mediatechnieken heb ik een poster gemaakt in Photoshop van de film “The Pursuit Of Happyness”.</p>
+            </div>
+        </div>
+        <div class="pdf-wrapper p-top">
+            <div class="pdf-inner">
+                <h1 class="pdf-text"><a class="link" target="_blank" href="/assets/files/Photoshop%20test%20Poster%20web.pdf">Film poster: The Pursuit Of Happyness</a></h1>
+                <iframe style="height: 90vh;width:100%;" src="/assets/files/Photoshop%20test%20Poster%20web.pdf"></iframe>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <h1>Hoogwaardig</h1>
+            </div>
+        </div>
+        <div class="scrolling-wrapper" id="wrapper-2">
+            <div class="card">
+                <div class="commentary" style="padding-top: 0;">
+                    <div class="commentary-inner">
+                        <div class="commentary-video" style="background-image: url({{ asset('assets/img/feedback_joris.png') }});background-size: contain;background-repeat: no-repeat;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="commentary">
+                    <div class="commentary-inner">
+                        <p style="top:0;left:0;text-align:left;max-width:100vw;" class="commentary-p"><span class="quote">Goed dat je met drie video's je proces laat zien en het lijkt mij prima geslaagd. Die met dat rode vlak slaat nergens op maar die cool is weer wel goed geslaagd. Wat mij betreft is deze opdracht prima.</span> – Joris Ariëns
+                        <br>Hierboven staat feedback van Joris over de opdrachten van de specialisatie mediatechnieken. Hiermee toon ik aan dat het erkend wordt door mensen uit het vakgebied.</p>
                     </div>
                 </div>
             </div>
@@ -89,79 +147,5 @@
         <div class="footer" style="background-image: url({{ asset('assets/img/IMG_0908.JPG') }});filter: url();">
             <div style="width: 35vw; left: 32.5vw;">a journey of discovery</div>
         </div>
-        <script>   
-            window.addEventListener("scroll",
-                function (event) {
-                    wrapper1();
-                    wrapper2();
-                    // console.log(document.getElementById("wrapper-1").scrollLeft);
-                },
-                false
-            );
-
-            var vWrapper1 = false;
-            function wrapper1() {
-                var element = document.getElementById("wrapper-1");
-                if(!vWrapper1){
-                    let scrollTop = window.scrollY,
-                    elementOffset = element.offsetTop,
-                    distance = (elementOffset - scrollTop);
-                    // console.log(distance);
-                    if(distance < 100) {
-                        vWrapper1 = true;
-                        scrollTo('wrapper-1', '500', '1500')
-                    }
-                }
-                else if(element.scrollLeft <= 0) {
-                    vWrapper1 = false;
-                }
-            }
-            var vWrapper2 = false;
-            function wrapper2() {
-                var element = document.getElementById("wrapper-2");
-                if(!vWrapper2){
-                    let scrollTop = window.scrollY,
-                    elementOffset = element.offsetTop,
-                    distance = (elementOffset - scrollTop);
-                    // console.log(distance);
-                    if(distance < 100) {
-                        vWrapper2 = true;
-                        scrollTo('wrapper-2', '500', '1500')
-                    }
-                }
-                else if(element.scrollLeft <= 0) {
-                    vWrapper2 = false;
-                }
-            }
-
-            function scrollTo(elementId, to, duration) {
-                let element = document.getElementById(elementId);
-                let start = element.scrollLeft,
-                change = to - start,
-                currentTime = 0,
-                increment = 20;
-
-                let animateScroll = function(){        
-                    currentTime += increment;
-                    let val = Math.easeInOutQuad(currentTime, start, change, duration);
-                    element.scrollLeft = val;
-                    if(currentTime < duration) {
-                        setTimeout(animateScroll, increment);
-                    }
-                };
-                animateScroll();
-            }
-
-            //t = current time
-            //b = start value
-            //c = change in value
-            //d = duration
-            Math.easeInOutQuad = function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t + b;
-                    t--;
-                return -c/2 * (t*(t-2) - 1) + b;
-            };
-        </script>
     </body>
 </html>
