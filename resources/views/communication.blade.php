adba440823d72984f551346cdec7f2e71b67540f
Shihaab <roushi02@gmail.com>
2020-06-21 15:56:13 +0200

    communication gevuld met inhoud

--- resources/views/communication.blade.php
+++ resources/views/communication.blade.php
@@ -16,132 +16,25 @@
             <a href="/" class="goBack">Shihaab Rouine</a>
             <h1>communication</h1>
         </div>
-
-        <div style="overflow: hidden;" class="scrolling-wrapper" id="wrapper-1">
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <h2 class="commentary-text" style="font-size: 5vw;">Professionele toolset</h2>
-                        <div style="background-image: url(https://www.adobe.com/content/dam/offers-homepage/us/en/homepage/lobby-acrobat-032320-mobile-1440x810.jpg.img.jpg);" class="commentary-firstimage"></div>
-                        <div style="background-image: url({{ asset('assets/img/xd.svg') }});" class="commentary-secondimage"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="text-wrapper">
+        <div class="text-wrapper p-top">
             <div class="text-inner">
+                <h1>Effectieve communicatie</h1>
                 <p>
-                    Voor het maken van de visuele ontwerpen heb ik de producten van Adobe Creative Cloud gebruikt: After Effects, Premiere Pro, Media Encoder, Photoshop en XD.
-                    <br><br>
-                    Voor webdevelopment gebruik ik Visual Studio Code, ik vind dit een fijn programma waar veel plugins voor zijn maar ook zonder is het super fijn om te gebruiken.
-                    <br>
-                    Verder ben ik bekent met Illustrator, Dimension, Audition, Dreamweaver en Fuse CC.
+                    Voor het maken van de applicatie My Brogrammers moest ik veel communiceren voor ik kon realiseren. Dit was iets dat ik niet gewend was op MBO of in het startsemester. Voor mij was het een totale verandering van werkwijze en een enorm leermoment. De communicatie was essentieel om succes te behalen, als ik vast loop en niet terug koppel en vragen stel kom ik niet verder. Hier leerde ik pas echt dat vragen stellen een enorm pluspunt is in een werknemer en of student. 
+                    <br><br>Communicatie was echt een van mijn slechte eigenschappen en ben in zoverre verbeterd dat ik, en het detacheringsbureau, …Iets van “blij mee” of “een van mijn beste eigenschappen nu”… 
                 </p>
             </div>
         </div>
-        <div class="text-wrapper p-top">
+        <div style="margin-bottom: 20vh;" class="text-wrapper p-top">
             <div class="text-inner">
-                <h1>Visuele ontwerpen</h1>
-                <p>Hier zijn twee Instagram story’s die ik heb gemaakt in Premiere Pro voor de specialisatie mediatechnieken, toen ik begon had ik de opdracht verkeerd gelezen en dacht ik dat de video max 50 seconden mocht duren (moest eigenlijk dus 15 seconden). Toen ik dit door had, had ik de video al geëxporteerd. </p>
-            </div>
-        </div>
-        <div class="scrolling-wrapper" id="wrapper-2">
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <div class="commentary-video">
-                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/IPdYlKKVFEQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <h2 class="commentary-text">15s versie rechts</h2>
-                        <p class="commentary-p">Ik moest dus de video inkorten en had nu ook de kans om andere aanpassingen te maken. Ik vond de video wel spannend maar te vrolijk voor wat ik wilde bereiken. Daarom heb ik voor de tweede versie een minder agressieve kleurencorrectie gekozen zodat het wat depressiever overkomt. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <div class="commentary-video">
-                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/13WU97gQedQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="text-wrapper p-top">
-            <div class="text-inner">
-                <p>Voor de specialisatie mediatechnieken werd ook gevraagd om je kennis met After Effects aan te tonen door de 3D camera tracking functie te gebruiken om een stuk tekst in de video te plaatsen. Hier heb ik twee video’s voor gemaakt. De eerste is een video met een simpel stukje tekst dat vaststaat alsof het in de ruimte zweeft. De andere drie zijn samen een proces om een stuk tekst achter de persoon op de muur te zetten.</p>
-                <br><p>Let op: de eerste video hieronder is een playlist.</p>
-            </div>
-        </div>
-        <div class="scrolling-wrapper" id="wrapper-2">
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <div class="commentary-video">
-                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/playlist?list=PLNq6lVSvI4Vt_JHpIYu3LS0FhTQwKAj3m&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <div class="commentary-video">
-                            <iframe style="width: 100%" height="100%" src="https://www.youtube.com/embed/VYGR2SaX1FY?mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="text-wrapper p-top">
-            <div class="text-inner">
-                <p>Toen ik net begon met het bedenken van een portfolio design heb ik wat ideeen kunnen documenteren. Dit heb ik toen gedaan voor de eerste portfolio review. </p>
-            </div>
-        </div>
-        <div class="pdf-wrapper p-top">
-            <div class="pdf-inner">
-                <h1 class="pdf-text"><a class="link" target="_blank" href="/assets/files/Portfolio%20Proces%20Shihaab%20Rouine%20v3.pdf">Portfolio proces</a></h1>
-                <iframe style="height: 90vh;width:100%;" src="/assets/files/Portfolio%20Proces%20Shihaab%20Rouine%20v3.pdf"></iframe>
-            </div>
-        </div>
-        <div class="text-wrapper p-top">
-            <div class="text-inner">
-                <p>Voor de specialisatie mediatechnieken heb ik een poster gemaakt in Photoshop van de film “The Pursuit Of Happyness”.</p>
-            </div>
-        </div>
-        <div class="pdf-wrapper p-top">
-            <div class="pdf-inner">
-                <h1 class="pdf-text"><a class="link" target="_blank" href="/assets/files/Photoshop%20test%20Poster%20web.pdf">Film poster: The Pursuit Of Happyness</a></h1>
-                <iframe style="height: 90vh;width:100%;" src="/assets/files/Photoshop%20test%20Poster%20web.pdf"></iframe>
-            </div>
-        </div>
-        <div class="text-wrapper p-top">
-            <div class="text-inner">
-                <h1>Hoogwaardig</h1>
-            </div>
-        </div>
-        <div class="scrolling-wrapper" id="wrapper-2">
-            <div class="card">
-                <div class="commentary" style="padding-top: 0;">
-                    <div class="commentary-inner">
-                        <div class="commentary-video" style="background-image: url({{ asset('assets/img/feedback_joris.png') }});background-size: contain;background-repeat: no-repeat;">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="commentary">
-                    <div class="commentary-inner">
-                        <p style="top:0;left:0;text-align:left;max-width:100vw;" class="commentary-p"><span class="quote">Goed dat je met drie video's je proces laat zien en het lijkt mij prima geslaagd. Die met dat rode vlak slaat nergens op maar die cool is weer wel goed geslaagd. Wat mij betreft is deze opdracht prima.</span> – Joris Ariëns
-                        <br>Hierboven staat feedback van Joris over de opdrachten van de specialisatie mediatechnieken. Hiermee toon ik aan dat het erkend wordt door mensen uit het vakgebied.</p>
-                    </div>
-                </div>
+                <h1>Stakeholders</h1>
+                <p>
+                Definitie stakeholder: <span class="quote">Stakeholders zijn personen of groepen die invloed hebben op een project, afdeling of organisatie en/of er door worden beïnvloed. Diverse doelgroepen die vanuit een specifiek belang bij de onderneming betrokken zijn zoals medewerkers, ondernemingsraad, aandeelhouders, financiers en klanten.</span>
+                <br><br>Dit semester: Ik, Bern-Jan (semester-coach) en mijn moeder? (Emotionele stakeholders) 
+                <br><br>Portfolio: Ik
+                <br><br>IKAI: Luca, Sander, Rafaël, Freek, Ivo, Ananda (IKAI) en Ik
+                <br><br>My Brogrammers (detacheringsbureau): Opdrachtgever/klant, eindgebruikers en ik.
+                </p>
             </div>
         </div>
         <div class="footer" style="background-image: url({{ asset('assets/img/IMG_0908.JPG') }});filter: url();">
