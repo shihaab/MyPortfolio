e41d3d3ace4e5e65b69eb7dc84d35d490a2f1679
Shihaab <roushi02@gmail.com>
2020-06-21 16:08:17 +0200

    professional identity bijna gevuld

--- resources/views/professional-identity.blade.php
+++ resources/views/professional-identity.blade.php
@@ -23,9 +23,52 @@
                     <div class="commentary-inner" style="height: 80vh">
                         <h2 style="position:unset;" class="commentary-text-side">Shihaab, a closer look</h2>
                         <p style="font-family: futura-pt, sans-serif;font-weight: 500;font-style: normal;font-size: 3vw;text-align: right;white-space: normal;max-width: 31vw;bottom: 0;position: absolute;">
-                            My personal and professional growth over the period of this semester
+                            Mijn persoonlijke en professionele groei gedurende dit semester
                         </p>
                         <div style="background-image: url({{ asset('assets/img/compressed/slide.gif') }});" class="commentary-image-side"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <h1>Professionele identiteit</h1>
+                <p>
+                    ...
+                </p>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <h1>Portfolio</h1>
+                <p>
+                    ...
+                </p>
+            </div>
+        </div>
+        <div class="text-wrapper p-top">
+            <div class="text-inner">
+                <h1>Groei</h1>
+                <p>
+                In de afgelopen 6 maanden ben ik enorm gegroeid, niet alleen in mijn professionele houding maar ook persoonlijk. Ik merk dat ik een mind-set heb aangenomen om te reflecteren wanneer er iets niet helemaal gebeurt zoals ik had gewild. Hier ben ik blij om omdat ik wel wist dat ik dit wilde doen maar het kwam er niet van. Nu reflecteer ik en bedenk ik wat ik beter kan doen de volgende keer. De volgende stap is het ook daadwerkelijk beter doen de volgende keer. Dit doet me ook denken aan een quote van Albert Einstein: “The definition of insanity is doing the same thing over and over again, but expecting different results.”.
+                <br><br>Zoals ik bij de leeruitkomst communication heb beschreven, was communicatie niet mijn beste vak. Dat is in dit semester echt vooruitgegaan.
+                </p>
+            </div>
+        </div>
+        <div class="scrolling-wrapper" id="wrapper-2">
+            <div class="card">
+                <div class="commentary" style="padding-top: 0;">
+                    <div class="commentary-inner">
+                        <div class="commentary-video" style="background-image: url({{ asset('assets/img/communicatie_feedback_bernd_jan.png') }});background-size: contain;background-repeat: no-repeat;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="commentary">
+                    <div class="commentary-inner">
+                        <p style="top:0;left:0;text-align:left;max-width:100vw;" class="commentary-p"><span class="quote">Je komt communicatief sterk en vaardig over in persoon, en in videogesprekken. De visuele presentatie van je werk komt ook sterk over, de structurele presentatie en inhoudelijke toelichting nu nog niet.</span> – Bernd-Jan Witkamp
+                        <br>Ik heb Bernd-Jan gevraagd om feedback te geven op mijn communicatie met hem dit semester.</p>
                     </div>
                 </div>
             </div>
