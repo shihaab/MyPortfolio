a4821a36f033004870bea11a466abf52a6dad624
Shihaab <roushi02@gmail.com>
2020-06-25 22:46:33 +0200

    Misschien wel de laatste commit Zoekresultaten Webresultaten 😭

--- resources/views/shihaab.blade.php
+++ resources/views/shihaab.blade.php
@@ -20,11 +20,17 @@
         <div class="text-wrapper p-top">
             <div class="text-inner">
                 <p>
-                    Hier boven staat het al, ik heb dit semester veel geleerd, echt veel. En er is van alles gebeurt. Een epidemie van een wereldwijd virus, thuis moeten werken en minder motivatie gehad. Mijn portfolio proces document dat corrupted werd en ik vanaf pagina 5 van de 16 opnieuw moest beginnen. 3 portfolio reviews achtergelopen en wist zeker dat ik het semester niet ging halen.
-                    <br>En heel veel stress gehad over werk samen met school in de laatste maand. Maar daar zijn we, einde semester 2. Nog een aantal te gaan.
-                    <br><br>Ik heb ook veel kleine dingen geleerd, dingen die ik zo niet kan benoemen maar mij toch weer wat wijzer hebben gemaakt. Dingen die ik de volgende keer anders zou doen, of dingen die ik heb geoefend en nu beter kan.
-                    <br>Ik heb blij dat het zo gelopen is, zou er niks aan verranderen. Dit was er voor mijn nodig om te groeien. En ik ben trots op mijn portfolio en wat ik neer heb gezet, maar het kan altijd beter en daar streef ik voor.
-                    <br><br>Ik hoop dat ik dit semester haal en verder kan om weer verder te leren en mezelf uit te dagen.
+                Niet veel mensen zullen dit lezen, dit was meer een toevoeging die niet per se in het portfolio wilde zetten.
+
+                <br><br>Hier boven staat het al, ik heb dit semester veel geleerd, echt veel. En er is van alles gebeurt. Een epidemie van een wereldwijd virus, thuis moeten werken en minder motivatie gehad. Mijn portfolio proces document dat corrupted werd en ik vanaf pagina 5 van de 16 opnieuw moest beginnen. 3 portfolio reviews achtergelopen en wist zeker dat ik het semester niet ging halen.
+                <br><br>En heel veel stress gehad over werk samen met school in de laatste maand. Maar daar zijn we, einde semester 2. Nog een aantal te gaan.
+
+                <br><br>Ik heb ook veel kleine dingen geleerd, dingen die ik zo niet kan benoemen maar mij toch weer wat wijzer hebben gemaakt. Dingen die ik de volgende keer anders zou doen, of dingen die ik heb geoefend en nu beter kan.
+                <br>Ik heb blij dat het zo gelopen is, zou er niets aan veranderen. Dit was er voor mijn nodig om te groeien. En ik ben trots op mijn portfolio en wat ik neer heb gezet, maar het kan altijd beter en daar streef ik voor.
+
+                <br><br>Ik hoop dat ik dit semester haal en verder kan om weer verder te leren en mezelf uit te dagen.
+
+                <br><br>En trouwens, ik heb echt veel plezier gehad met het maken van dit portfolio. Ik vind het geweldig om projecten te starten, gekke ideeën uitproberen. Echter maak ik ze dan niet helemaal af... Dat vind ik zelf altijd jammer maar heb er op dat moment gewoon geen motivatie voor. Ik ben blij dat ik dit heb afgemaakt en het ook nog eens wat is geworden dat ik mooi vind en iets waar ik trots op kan zijn. Ik wilde altijd een portfolio met werk om te kunnen laten zien maar had toen alleen halve projecten. Nu heb ik een portfolio dat werk op zich zelf is en heb dit semester ook geleerd dat half werk niet niets is en er nog steeds iets van hebt opgenomen dat waardevol is om te laten zien.
                 </p>
             </div>
         </div>
